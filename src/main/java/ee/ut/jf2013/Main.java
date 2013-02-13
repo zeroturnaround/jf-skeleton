249aa8ab60e78d08b7339711739e436a22090b3b
Jaroslav Judin <judin.jaroslav@gmail.com>
2013-02-13 13:46:37 +0200

    Use default Main class which is used for maven executable jar.

--- src/main/java/ee/ut/jf2013/Main.java
+++ src/main/java/ee/ut/jf2013/Main.java
@@ -1,11 +1,14 @@
 package ee.ut.jf2013;
 
-import ee.ut.jf2013.homework1.Homework;
+import ee.ut.jf2013.homework1.BinaryFileReverser;
+
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        Homework homework = new Homework();
-
-        System.out.println(homework.square(4));
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            throw new RuntimeException("Please specify file name");
+        }
+        new BinaryFileReverser().reverseBinaryFileContentInSingleFile(args[0]);
     }
 }
