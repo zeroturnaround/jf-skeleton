a68066f2aaa18f6013e08398be065407d2d09a91
Jaroslav Judin <judin.jaroslav@gmail.com>
2013-02-20 21:45:57 +0200

    Extract Jetty server initialization.

---
+++ src/main/java/ee/ut/jf2013/homework2/JettyServer.java
@@ -0,0 +1,43 @@
+package ee.ut.jf2013.homework2;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.channels.SocketChannel;
+import java.util.Map;
+import java.util.Set;
+
+import static ee.ut.jf2013.homework2.SocketServer.encode;
+
+public class JettyServer {
+
+    static void startJetty(final Map<String, SocketChannel> clients) throws Exception {
+        Server server = new Server(8080);
+        server.setHandler(new AbstractHandler() {
+            @Override
+            public void handle(String s, Request request, HttpServletRequest httpRequest, HttpServletResponse httpResponse) throws IOException, ServletException {
+                request.setHandled(true);
+                String author = httpRequest.getHeader("author");
+                if (author == null) {
+                    httpResponse.getWriter().println("TEST -> WORKS");
+                    httpResponse.setStatus(HttpServletResponse.SC_OK);
+                    return;
+                }
+                Set<String> params = httpRequest.getParameterMap().keySet();
+                String message = params.isEmpty() ? "" : params.iterator().next();
+                String content = author + ": " + message;
+                System.out.println(content);
+                for (SocketChannel channel : clients.values()) {
+                    channel.write(encode(content));
+                }
+            }
+        });
+        server.start();
+    }
+
+}
--- src/main/java/ee/ut/jf2013/homework2/SocketServer.java
+++ src/main/java/ee/ut/jf2013/homework2/SocketServer.java
@@ -1,17 +1,13 @@
 package ee.ut.jf2013.homework2;
 
-import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.AbstractHandler;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
-import java.nio.channels.*;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.nio.charset.CharacterCodingException;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
@@ -20,14 +16,16 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import static ee.ut.jf2013.homework2.JettyServer.startJetty;
+
 public class SocketServer {
 
-    private static Map<String, SocketChannel> clients = new HashMap<>();
-    private static CharsetEncoder encoder = Charset.forName("UTF-8").newEncoder();
+    private static final Map<String, SocketChannel> clients = new HashMap<>();
+    private static final CharsetEncoder encoder = Charset.forName("UTF-8").newEncoder();
     private static final CharsetDecoder decoder = Charset.forName("UTF-8").newDecoder();
 
     public static void main(String[] args) throws Exception {
-        startJetty();
+        startJetty(clients);
 
         try (ServerSocketChannel server = ServerSocketChannel.open()) {
             server.configureBlocking(false);
@@ -54,8 +52,7 @@ public class SocketServer {
             SocketChannel client = srv.accept();
             client.configureBlocking(false);
             client.register(key.selector(), SelectionKey.OP_READ);
-        }
-        else if (key.isReadable()) {
+        } else if (key.isReadable()) {
             SocketChannel client = (SocketChannel) key.channel();
             ByteBuffer buf = ByteBuffer.allocate(10);
             client.read(buf);
@@ -83,31 +80,7 @@ public class SocketServer {
         }
     }
 
-    private static void startJetty() throws Exception {
-        Server server = new Server(8080);
-        server.setHandler(new AbstractHandler() {
-            @Override
-            public void handle(String s, Request request, HttpServletRequest httpRequest, HttpServletResponse httpResponse) throws IOException, ServletException {
-                request.setHandled(true);
-                String author = httpRequest.getHeader("author");
-                if (author == null) {
-                    httpResponse.getWriter().println("TEST -> WORKS");
-                    httpResponse.setStatus(HttpServletResponse.SC_OK);
-                    return;
-                }
-                Set<String> params = httpRequest.getParameterMap().keySet();
-                String message = params.isEmpty() ? "" : params.iterator().next();
-                String content = author + ": " + message;
-                System.out.println(content);
-                for (SocketChannel channel : clients.values()) {
-                    channel.write(encode(content));
-                }
-            }
-        });
-        server.start();
-    }
-
-    private static ByteBuffer encode(String message) throws CharacterCodingException {
+    static ByteBuffer encode(String message) throws CharacterCodingException {
         return encoder.encode(CharBuffer.wrap(message + "\r\n"));
     }
 
