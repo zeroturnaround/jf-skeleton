cb58218b7a894380b39a3361a71ecb8f79bbbed8
Jaroslav Judin <judin.jaroslav@gmail.com>
2013-02-21 09:36:38 +0200

    Remove client from the build.

--- src/main/java/ee/tut/fundamentals/chat/MessageListener.java
+++
@@ -1,9 +0,0 @@
-package ee.tut.fundamentals.chat;
-
-public interface MessageListener {
-
-  /**
-   * Triggered when an IM is received from the server
-   */
-  public void onMessage(String msg);
-}
