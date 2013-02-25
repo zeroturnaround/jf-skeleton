22f3acf94c59f00c35f108d80addf4ea0869a02c
Jaroslav Judin <judin.jaroslav@gmail.com>
2013-02-25 22:59:33 +0200

    Add file watcher.

--- src/main/java/ee/ut/jf2013/homework3/DirectoryReader.java
+++ src/main/java/ee/ut/jf2013/homework3/DirectoryReader.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import static java.nio.file.Files.*;
 import static java.nio.file.LinkOption.NOFOLLOW_LINKS;
 import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import static java.nio.file.StandardWatchEventKinds.*;
 import static java.util.Collections.unmodifiableMap;
 
 public class DirectoryReader {
@@ -15,10 +16,14 @@ public class DirectoryReader {
     private final static FileSystem fileSystem = FileSystems.getDefault();
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         Map<String, String> parameters = readParameters(args);
         Path source = validateSourceDirectory(parameters);
         Path target = checkAndCreateTargetDirectory(parameters);
+
+        WatchService service = fileSystem.newWatchService();
+
+        source.register(service, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
 
         try (DirectoryStream<Path> sourceDir = Files.newDirectoryStream(source);
              DirectoryStream<Path> targetDir = Files.newDirectoryStream(target)) {
@@ -26,11 +31,9 @@ public class DirectoryReader {
                 if (!isDirectory(entry)) {
                     Path targetFile = target.resolve(entry.getFileName());
                     if (notExists(targetFile)) {
-                        Files.copy(entry, targetFile);
-                        System.out.println("Created new file: " + targetFile);
+                        createFile(target, entry);
                     } else if (getLastModifiedTime(targetFile).compareTo(getLastModifiedTime(entry)) < 0) {
-                        Files.copy(entry, targetFile, REPLACE_EXISTING);
-                        System.out.println("Updated file: " + targetFile);
+                        updateFile(target, entry);
                     }
                 }
             }
@@ -38,6 +41,38 @@ public class DirectoryReader {
             deleteNotExistingFiles(source, targetDir);
         }
 
+        WatchKey key;
+        while ((key = service.take()).isValid()) {
+            for (WatchEvent<?> event : key.pollEvents()) {
+                WatchEvent.Kind<Path> kind = (WatchEvent.Kind<Path>) event.kind();
+                Path entry = source.resolve((Path) event.context());
+                if (ENTRY_CREATE == kind) {
+                    createFile(target, entry);
+                } else if (ENTRY_MODIFY == kind) {
+                    updateFile(target, entry);
+                } else if (ENTRY_DELETE == kind) {
+                    deleteFile(target, entry);
+                }
+            }
+            key.reset();
+        }
+
+    }
+
+    private static void deleteFile(Path target, Path entry) throws IOException {
+        Path path = target.resolve(entry.getFileName());
+        Files.delete(path);
+        System.out.println("delete file from target which has been removed from source " + path);
+    }
+
+    private static void updateFile(Path target, Path entry) throws IOException {
+        Path update = Files.copy(entry, target.resolve(entry.getFileName()), REPLACE_EXISTING);
+        System.out.println("Updated file: " + update);
+    }
+
+    private static void createFile(Path target, Path entry) throws IOException {
+        Path copy = Files.copy(entry, target.resolve(entry.getFileName()));
+        System.out.println("Created new file: " + copy);
     }
 
     private static void deleteNotExistingFiles(Path source, DirectoryStream<Path> targetDir) throws IOException {
