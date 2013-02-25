e425d6e8471a129b5f3239a6a13daea650928d47
Jaroslav Judin <judin.jaroslav@gmail.com>
2013-02-25 20:21:41 +0200

    Check initial directories existence.

---
+++ src/main/java/ee/ut/jf2013/homework3/DirectoryReader.java
@@ -0,0 +1,53 @@
+package ee.ut.jf2013.homework3;
+
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.nio.file.Files.notExists;
+import static java.nio.file.LinkOption.NOFOLLOW_LINKS;
+import static java.util.Collections.unmodifiableMap;
+
+public class DirectoryReader {
+
+    private final static FileSystem fileSystem = FileSystems.getDefault();
+
+
+    public static void main(String[] args) throws IOException {
+        Map<String, String> parameters = readParameters(args);
+        validateSourceDirectory(parameters);
+        checkAndCreateTargetDirectory(parameters);
+    }
+
+    private static Path checkAndCreateTargetDirectory(Map<String, String> parameters) throws IOException {
+        Path target = fileSystem.getPath(parameters.get("target"));
+        if (notExists(target, NOFOLLOW_LINKS)) {
+            System.out.println("New target directory was successfully created: " + Files.createDirectories(target));
+        }
+        return target;
+    }
+
+    private static void validateSourceDirectory(Map<String, String> parameters) throws IOException {
+        if (notExists(fileSystem.getPath(parameters.get("source")), NOFOLLOW_LINKS)) {
+            throw new IOException("Source directory doesn't exist!");
+        }
+    }
+
+
+    private static Map<String, String> readParameters(final String[] parameters) {
+        if (parameters.length < 2) {
+            throw new RuntimeException("source or target directories weren't defined in program arguments.");
+        }
+        return unmodifiableMap(new HashMap<String, String>(2) {
+            {
+                put("source", parameters[0]);
+                put("target", parameters[1]);
+            }
+        });
+    }
+
+}
