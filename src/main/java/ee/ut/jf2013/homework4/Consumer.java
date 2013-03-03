68625f2e74a82f4ade77df479d67b0373b8eafb8
Jaroslav Judin <judin.jaroslav@gmail.com>
2013-03-03 18:26:46 +0200

    Remove rubbish.

--- src/main/java/ee/ut/jf2013/homework4/Consumer.java
+++ src/main/java/ee/ut/jf2013/homework4/Consumer.java
@@ -1,12 +1,5 @@
 package ee.ut.jf2013.homework4;
 
-/**
- * Created with IntelliJ IDEA.
- * User: jaros
- * Date: 3/3/13
- * Time: 6:23 PM
- * To change this template use File | Settings | File Templates.
- */
 class Consumer extends User {
     Consumer(TunedBlockingQueue queue) {
         super(queue);
