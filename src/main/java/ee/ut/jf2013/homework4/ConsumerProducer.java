cd14db4d3fc0c088d776154b4cd3180e78d5b820
Jaroslav Judin <judin.jaroslav@gmail.com>
2013-03-03 14:47:35 +0200

    Interrupt threads for proper program termination.

--- src/main/java/ee/ut/jf2013/homework4/ConsumerProducer.java
+++ src/main/java/ee/ut/jf2013/homework4/ConsumerProducer.java
@@ -1,6 +1,8 @@
 package ee.ut.jf2013.homework4;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -9,60 +11,72 @@ public class ConsumerProducer {
     public static final int PRODUCERS_AMOUNT = 6;
     public static final int CONSUMERS_AMOUNT = 8;
 
-    public static void main(String[] args) {
-        //final BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(15);
+    public static void main(String[] args) throws InterruptedException {
         final TunedBlockingQueue queue = new TunedBlockingQueue(15);
 
+        List<Thread> threads = new ArrayList<>();
 
         for (int i = 0; i < PRODUCERS_AMOUNT; i++) {
-            new Thread(new Producer(queue)).start();
+            threads.add(new Producer(queue));
         }
 
         for (int i = 0; i < CONSUMERS_AMOUNT; i++) {
-            new Thread(new Consumer(queue)).start();
+            threads.add(new Consumer(queue));
         }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        Thread.sleep(5000);
+
+        for (Thread thread : threads) {
+            thread.interrupt();
+            //thread.join();
+        }
+
     }
 }
 
-class Producer implements Runnable {
+class Producer extends Thread {
     private final TunedBlockingQueue queue;
 
-    public Producer(TunedBlockingQueue queue) {
+    Producer(TunedBlockingQueue queue) {
         this.queue = queue;
     }
 
     @Override
     public void run() {
-        while (true) {
+        while (!isInterrupted()) {
             int element = ThreadLocalRandom.current().nextInt();
             queue.add(element);
             System.out.println(Thread.currentThread().getName() + " added " + element);
         }
+        System.out.println(Thread.currentThread().getName() + " finished it's execution");
     }
 }
 
-class Consumer implements Runnable {
+class Consumer extends Thread {
     private final TunedBlockingQueue queue;
 
-    public Consumer(TunedBlockingQueue queue) {
+    Consumer(TunedBlockingQueue queue) {
         this.queue = queue;
     }
 
     @Override
     public void run() {
-        while (true) {
+        while (!isInterrupted()) {
             Integer integer = queue.get();
             System.out.println(Thread.currentThread().getName() + " got " + integer);
         }
+        System.out.println(Thread.currentThread().getName() + " finished it's execution");
     }
 }
 
 class TunedBlockingQueue {
 
     private final int size;
-
-    private final Stack<Integer> queue = new Stack();
-
+    private final Stack<Integer> queue = new Stack<>();
 
     TunedBlockingQueue(int size) {
         this.size = size;
